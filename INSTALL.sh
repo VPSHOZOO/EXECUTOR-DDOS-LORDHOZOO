a6fdfcc339c629589df090ad17442d6653c64271
VPSHOZOO <lordhozoo891@gmail.com>
2025-04-23 10:03:51 +0700

    Create INSTALL.sh

---
+++ INSTALL.sh
@@ -0,0 +1,46 @@
+#!/bin/bash/EXECUTOR/NULL
+
+e="echo -e"
+clear
+clear
+$e "[INSTALL DDOS TELEGRAM HACKED LORDHOZOO ]"
+$e "[SABAR LOADING........]"
+sleep 0.8
+sudo apt-get update && apt  upgrade -y
+clear
+sudo apt-get install git -y
+clear
+sudo apt install nodejs -y
+clear
+sudo apt install npm -y
+clear
+npm node-telegram-bot-ap
+clear
+npm install moment
+clear
+npm install axios 
+clear
+npm install net
+clear
+npm install tls
+clear
+npm install http
+clear
+npm install https
+clear
+npm install socks
+clear
+npm install socks-proxy-agent
+clear
+npm install cloudscraper
+clear
+npm install cheerio
+clear
+npm install puppeteer
+
+$e " [PENGINSTALAN SELESAI ...]"
+sleep 2
+clear
+$e "[WELCome ddos executor hacked lordozoo"
+clear
+node EXECUTOR.js
